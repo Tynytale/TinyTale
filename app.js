98ee5c612f25d90c2cf84049bafa06da5d77ea33
Tynytale <rhythmicbeats.hub@gmail.com>
2026-07-29 10:19:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const button = document.getElementById("enterBtn");
+const transition = document.getElementById("transition");
 
+button.addEventListener("click", () => {
+
+    transition.style.opacity = "1";
+
+    setTimeout(() => {
+        window.location.href = "pages/home.html";
+    }, 1200);
+
+});
